b857328cc43d4e7d28412e5155718308828a35b2
Angela <reallygooday@users.noreply.github.com>
2018-04-08 13:58:18 +0100

    adding file

---
+++ app.rb
@@ -0,0 +1,134 @@
+require "sinatra"
+
+def comment
+end
+
+get "/" do 
+	erb :home
+end
+
+post "/" do
+	@questionNumber = params[:questionNumber]
+	
+	if params[:selection] == "earth"
+		erb :earth
+	elsif params[:selection] == "erlang"
+		erb :erlang
+	elsif params[:selection] == "saturn"
+		erb :saturn
+	elsif params[:selection] == "jupiter"
+		erb :jupiter
+	elsif params[:selection] == "mars"
+		erb :mars
+	else
+		erb :home	
+	end		
+end	
+
+post "/earth" do
+	@questionNumber = params[:questionNumber].to_i + 1
+	@selection = params[:selection]
+	
+	if params[:questionNumber] == "1" || params[:questionNumber] == "3"
+		if params[:selection] == "stay"
+			erb :earth
+		elsif params[:selection] == "leave"
+			erb :home
+		else 
+			erb :earth
+		end
+	else
+		erb :earth
+	end
+end
+
+
+post "/erlang" do
+	@questionNumber = params[:questionNumber].to_i + 1
+	@selection = params[:selection]
+	
+	if params[:questionNumber] == "1" || params[:questionNumber] == "3"
+		if params[:selection] == "stay"
+			erb :erlang
+		elsif params[:selection] == "leave"
+			erb :home
+		else 
+			erb :erlang
+		end
+	else
+		erb :erlang
+	end
+end
+
+post "/saturn" do
+	@questionNumber = params[:questionNumber].to_i + 1
+	@selection = params[:selection]
+	
+	if params[:questionNumber] == "1" || params[:questionNumber] == "3"
+		if params[:selection] == "stay"
+			erb :saturn
+		elsif params[:selection] == "leave"
+			erb :home
+		else 
+			erb :saturn
+		end
+	else
+		erb :saturn
+	end
+end
+
+
+post "/jupiter" do
+	@questionNumber = params[:questionNumber].to_i + 1
+	@selection = params[:selection]
+	
+	if params[:questionNumber] == "1" || params[:questionNumber] == "3"
+		if params[:selection] == "stay"
+			erb :jupiter
+		elsif params[:selection] == "leave"
+			erb :home
+		else 
+			erb :jupiter
+		end
+	else
+		erb :erlang
+	end
+end
+
+post "/mars" do
+	@questionNumber = params[:questionNumber].to_i + 1
+	@selection = params[:selection]
+	
+	if params[:questionNumber] == "1" || params[:questionNumber] == "3"
+		if params[:selection] == "stay"
+			erb :mars
+		elsif params[:selection] == "leave"
+			erb :home
+		else 
+			erb :mars
+		end
+	else
+		erb :mars
+	end
+end
+
+
+
+get "/about" do
+	erb :about
+end
+
+
+get "/team" do
+	erb :team
+end
+
+
+
+
+
+
+
+
+
+
